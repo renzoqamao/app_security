d75747f51afef3feca293c86a74def4bc6515df4
renzoqamao <renzoquispeamao@gmail.com>
2025-12-09 23:54:55 -0500

    create api key filter to test

---
+++ src/main/java/com/custom/app_security/security/ApiKeyFilter.java
@@ -0,0 +1,38 @@
+package com.custom.app_security.security;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ApiKeyFilter extends OncePerRequestFilter {
+
+    private static final String API_KEY= "myKey";
+    private static final String API_KEY_HEADER= "api_key";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        
+        
+        try {
+            final var apiKeyOpt = Optional.of(request.getHeader(API_KEY_HEADER));
+            final var apiKey = apiKeyOpt.orElseThrow( ()-> new BadCredentialsException("No header api key"));
+            if (!apiKey.equals(API_KEY)) {
+                throw new BadCredentialsException("Invalida api key");
+            }
+        } catch (Exception e) {
+            throw new BadCredentialsException("Invalida api key");
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+
+}
--- src/main/java/com/custom/app_security/security/SecurityConfig.java
+++ src/main/java/com/custom/app_security/security/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+        http.addFilterBefore(new ApiKeyFilter(), BasicAuthenticationFilter.class);
         var requestHandler = new CsrfTokenRequestAttributeHandler();
         requestHandler.setCsrfRequestAttributeName("_csrf");
 
